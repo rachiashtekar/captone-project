a8cf973bde0dc6a56111f9d4425e4173c1b6134b
rachi <rachiashtekar345@gmail.com>
2024-03-07 00:51:55 +0530

    added css in Timer component

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 function News(){
-    // const [News,setNews]=useState(null);
+    const [News,setNews]=useState(null);
     // useEffect(()=>{
     //     fetch("https://api.worldnewsapi.com/search-news?text=nagpur&language=en&api-key=602b6c39c9e1421581f7778247c07437")
     //     .then((data) => data.json())
@@ -12,12 +12,12 @@ function News(){
 
     return (
         <div style={{marginTop:"10px",height:"500px",width:"350px",position:"absolute",top:"10px",left:"1160px",background:"white",borderRadius:"19px",alignItems:"centre"}}>
-       {/* {News? <img style={{height:"300px",width:"340px",position:"relative",left:"5px",top:"5px",right:"5px"}} alt='img' src={News.news[0].image}/>:<></>}
-       {News?<h1 style={{color:"black",background:"white",fontSize:"30px"}}>{News.news[0].title}</h1>:<></>}
-       {News ? <p style={{color:"black",background:"white",fontSize:"18px"}}>{News.news[0].text.slice(0, 350)}...</p> : <></>} */}
-       * {News? <img style={{alignItems:"center"}} alt='img' src="https://tse4.mm.bing.net/th?id=OIP.Rk9mQSpeenSA9qnGCimtZQHaEG&pid=Api&P=0&h=180"/>:<></>}
+     {/* {News? <img style={{height:"300px",width:"340px",position:"relative",left:"5px",top:"5px",right:"5px"}} alt='img' src={News.news[0].image}/>:<></>}
+     {News?<h1 style={{color:"black",background:"white",fontSize:"30px"}}>{News.news[0].title}</h1>:<></>}
+     {News ? <p style={{color:"black",background:"white",fontSize:"18px"}}>{News.news[0].text.slice(0, 370)}...</p> : <></>} */}
+       {News? <img style={{alignItems:"center"}} alt='img' src="https://tse4.mm.bing.net/th?id=OIP.Rk9mQSpeenSA9qnGCimtZQHaEG&pid=Api&P=0&h=180"/>:<></>}
        {News? <h1 style={{color:"black",background:"white"}}>If we read every piece of content put in front of us each day, we’d get nothing else done. Instead, we skim.</h1>: <></>} 
-       {News? <p style={{color:"black",background:"white"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled i </p>:<></>} 
+       {News? <p style={{color:"black",background:"white"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled i </p>:<></>}  
 
         </div>
 
--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,128 +1,190 @@
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import React, { useState } from "react";
+import Timerimg from "../asset/Timerimg.png";
+import Timerimg2 from "../asset/Timerimg2.png";
 export default function Timer() {
   const [seconds, setSeconds] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
 
   return (
-    <div
-      style={{
-        background: "#1E2343",
-        height: "200px",
-        width: "73vw",
-        margin: "10px",
-        borderRadius: "19px",
-        display: "flex",
-        justifyContent: "space-between",
-      }}
-    >
-      <CountdownCircleTimer
-        isPlaying={isPlaying}
-        duration={seconds}
-        colors={["#FF6A6A"]}
-      >
-        {({ remainingTime }) => {
-          if (remainingTime === 0) {
-            setIsPlaying(false);
-            // alert("Time is up!");
-            return (
-              <div style={{ color: "white", fontSize: "30px" }}>
-                Time is up!
-              </div>
-            );
-          } else {
-            return (
-              <>
-                <div style={{ color: "white", fontSize: "30px" }}>
-                  {Math.floor(remainingTime / 3600)} :
-                </div>{" "}
-                {/* 1 hour = 3600 seconds */}{" "}
-                {/* 3661 secodonds = 1 hour 1 minute 1 second */}
-                <div style={{ color: "white", fontSize: "30px" }}>
-                  {" "}
-                  {Math.floor((remainingTime % 3600) / 60)} :
-                </div>
-                <div style={{ color: "white", fontSize: "30px" }}>
-                  {remainingTime % 60}{" "}
-                </div>
-              </>
-            );
-          }
+    <div>
+      <div
+        style={{
+          background: "#1E2343",
+          height: "200px",
+          width: "73vw",
+          margin: "10px",
+          borderRadius: "19px",
+          display: "flex",
+          justifyContent: "space-between",
         }}
-      </CountdownCircleTimer>
-      <div style={{ display: "flex",position:"relative",}}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            // justifyContent: "space-between",
-          }}
-        >
-          <p style={{ color: "#949494", fontSize: "26px" }}>Seconds</p>
-          <button onClick={() => setSeconds((prev) => prev + 1)}>
-            + Second
-          </button>
-          <button
-            onClick={() => {
-              if (seconds > 0) {
-                setSeconds((prev) => prev - 1);
+      >
+        <div style={{ margin: "10px", marginLeft: "50px" }}>
+          <CountdownCircleTimer
+            isPlaying={isPlaying}
+            duration={seconds}
+            colors={["#FF6A6A"]}
+            // style={{ margin: '200px' }}
+          >
+            {({ remainingTime }) => {
+              if (remainingTime === 0) {
+                setIsPlaying(false);
+                // alert("Time is up!");
+                return (
+                  <div style={{ color: "white", fontSize: "30px" }}>
+                    Time is up!
+                  </div>
+                );
+              } else {
+                return (
+                  <>
+                    <div style={{ color: "white", fontSize: "30px" }}>
+                      {Math.floor(remainingTime / 3600)} :
+                    </div>{" "}
+                    {/* 1 hour = 3600 seconds */}{" "}
+                    {/* 3661 secodonds = 1 hour 1 minute 1 second */}
+                    <div style={{ color: "white", fontSize: "30px" }}>
+                      {" "}
+                      {Math.floor((remainingTime % 3600) / 60)} :
+                    </div>
+                    <div style={{ color: "white", fontSize: "30px" }}>
+                      {remainingTime % 60}{" "}
+                    </div>
+                  </>
+                );
               }
             }}
-          >
-            - Second
-          </button>
+          </CountdownCircleTimer>
         </div>
-
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            // justifyContent: "space-between",
-          }}
-        >
-          <p style={{ color: "#949494", fontSize: "26px" }}>Minutes</p>
-          <button onClick={() => setSeconds((prev) => prev + 60)}>
-            + Minute
-          </button>
-          <button
-            onClick={() => {
-              if (seconds >= 60) {
-                setSeconds((prev) => prev - 60);
-              }
+        <div style={{ display: "flex" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              position: "absolute",
+              left: "400px",
+              margin: "20px",
             }}
           >
-            - Minute
-          </button>
-        </div>
+            <p style={{ color: "#949494", fontSize: "26px" }}>Seconds</p>
+            <button
+              style={{ background: "none", border: "none" }}
+              onClick={() => setSeconds((prev) => prev + 1)}
+            >
+              <img src={Timerimg} />
+            </button>
+            <button
+              style={{ background: "none", border: "none" }}
+              onClick={() => {
+                if (seconds > 0) {
+                  setSeconds((prev) => prev - 1);
+                }
+              }}
+            >
+              <img src={Timerimg2} />
+            </button>
+          </div>
 
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            // justifyContent: "space-between",
-          }}
-        >
-          <p style={{ color: "#949494", fontSize: "26px" }}>Hours</p>
-          <button onClick={() => setSeconds((prev) => prev + 3600)}>
-            + Hour
-          </button>
-          <button
-            onClick={() => {
-              if (seconds >= 3600) {
-                setSeconds((prev) => prev - 3600);
-              }
+          <p
+            style={{
+              fontSize: "50px",
+              color: "white",
+              position: "absolute",
+              left: "550px",
+              top: "520px",
             }}
           >
-            - Hour
-          </button>
+            :
+          </p>
+
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              position: "absolute",
+              left: "600px",
+              margin: "20px",
+            }}
+          >
+            <p style={{ color: "#949494", fontSize: "26px" }}>Minutes</p>
+            <button
+              style={{ background: "none", border: "none" }}
+              onClick={() => setSeconds((prev) => prev + 60)}
+            >
+              <img src={Timerimg} />
+            </button>
+            <button
+              style={{ background: "none", border: "none" }}
+              onClick={() => {
+                if (seconds >= 60) {
+                  setSeconds((prev) => prev - 60);
+                }
+              }}
+            >
+              <img src={Timerimg2} />
+            </button>
+          </div>
+
+          <p
+            style={{
+              fontSize: "50px",
+              color: "white",
+              position: "absolute",
+              left: "750px",
+              top: "520px",
+            }}
+          >
+            :
+          </p>
+
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              position: "absolute",
+              left: "800px",
+              margin: "20px",
+            }}
+          >
+            <p style={{ color: "#949494", fontSize: "26px" }}>Hours</p>
+            <button
+              style={{ background: "none", border: "none" }}
+              onClick={() => setSeconds((prev) => prev + 3600)}
+            >
+              <img src={Timerimg} />
+            </button>
+            <button
+              style={{ background: "none", border: "none" }}
+              onClick={() => {
+                if (seconds >= 3600) {
+                  setSeconds((prev) => prev - 3600);
+                }
+              }}
+            >
+              <img src={Timerimg2} />
+            </button>
+          </div>
         </div>
       </div>
-
-      <div>
-        <button disabled={isPlaying} onClick={() => setIsPlaying(true)}>
-          Play
+      <div style={{ position: "absolute", top: "650px", left: "450px" }}>
+        <button
+          style={{
+            height: "40px",
+            width: "400px",
+            background: "#FF6A6A",
+            borderRadius: "20px",
+            border: "none",
+          }}
+          disabled={isPlaying}
+          onClick={() => setIsPlaying(true)}
+        >
+          <span style={{ color: "white", fontSize: "20px" }}>Start</span>
         </button>
       </div>
+      {/* </div>  */}
     </div>
   );
 }
